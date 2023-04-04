c24b37794da9d02d9ce1d5c4cffc7a5abb7ee0ee
Kirill <kaxyonov@gmail.com>
2023-04-04 11:50:36 +0300

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+
+public class Main {
+    public static void main(String[] args) {
+        int cost = 34987;
+        int quantity = 20;
+        int bonus = cost / quantity;
+        System.out.println(bonus + " Бонусных миль");
+
+    }
+}
